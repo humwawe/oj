c5a4c3d257a128439423efbe5d5d9e8cb933aa4b
humwawe <wawe8963098@163.com>
2023-12-21 23:35:51 +0800

    atcoder lib practice

---
+++ hum/atcoder/c_library_practice/task_a.py
@@ -0,0 +1,58 @@
+class DJSet:
+
+  def __init__(self, n):
+    self.p = [-1] * n
+
+  def find(self, x):
+    if self.p[x] < 0:
+      return x
+    nx = x
+    while self.p[x] >= 0:
+      x = self.p[x]
+    while nx != x:
+      self.p[nx], nx = x, self.p[nx]
+    return x
+
+  def merge(self, x, y):
+    x = self.find(x)
+    y = self.find(y)
+    if x != y:
+      if self.p[y] < self.p[x]:
+        x, y = y, x
+      self.p[x] += self.p[y]
+      self.p[y] = x
+    return x == y
+
+  def equiv(self, x, y):
+    return self.find(x) == self.find(y)
+
+  def count(self):
+    cnt = 0
+    for i in self.p:
+      if i < 0:
+        cnt += 1
+    return cnt
+
+  def size(self, x):
+    return -self.p[self.find(x)]
+
+  def to_bucket(self):
+    n = len(self.p)
+    ret = [[] for _ in range(n)]
+    for i in range(n):
+      r = self.find(i)
+      ret[r].append(i)
+    return ret
+
+
+n, q = map(int, input().split())
+dj = DJSet(n)
+for _ in range(q):
+  t, u, v = map(int, input().split())
+  if t == 0:
+    dj.merge(u, v)
+  else:
+    if dj.equiv(u, v):
+      print(1)
+    else:
+      print(0)
