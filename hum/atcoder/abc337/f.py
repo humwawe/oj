5cbea6dfbc847125b40ba3eec352ac8da212cc21
hum <wawe8963098@163.com>
2024-02-01 10:58:17 +0800

    abc337

--- hum/atcoder/abc337/f.py
+++ hum/atcoder/abc337/f.py
@@ -2,22 +2,23 @@ from collections import Counter
 
 n, m, k = map(int, input().split())
 a = list(map(int, input().split()))
-tmp = Counter(a)
+f = Counter(a)
 a += a
-r = -1
+r = 0
 
 cnt = Counter()
 res = 0
+now = 0
 for l in range(n):
-  while r - l + 2 <= n and len(cnt) + (a[r + 1] not in cnt) <= m:
-    r += 1
-    if a[r] not in cnt:
-      res += min(tmp[a[r]], k)
+  while r < n + l and now < m:
     cnt[a[r]] += 1
+    if (cnt[a[r]] - 1) % k == 0:
+      now += 1
+      res += min(f[a[r]] - (cnt[a[r]] - 1), k)
+    r += 1
 
   print(res)
-  if cnt[a[l]] == 1:
-    res -= min(tmp[a[l]], k)
-    del cnt[a[l]]
-  else:
-    cnt[a[l]] -= 1
+  cnt[a[l]] -= 1
+  if cnt[a[l]] % k == 0:
+    now -= 1
+    res -= min(f[a[l]] - cnt[a[l]], k)
